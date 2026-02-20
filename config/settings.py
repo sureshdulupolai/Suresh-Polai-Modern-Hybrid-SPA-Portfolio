c328813aaafbde5ee56ee9d2cc0468611a8ad1a4
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2026-02-21 01:00:26 +0530

    adding updates on setting to finish

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-manual-creation-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'suresh-polai.onrender.com']
 
 
 # Application definition
